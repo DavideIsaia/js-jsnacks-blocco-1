db02316f8fc8305f5c1e40cbdfe1ec452e6a1685
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-19 14:29:52 +0200

    snack 3 sviluppato

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,16 @@
 // Calcola la somma e la media dei primi 10 numeri.
+
+// dichiaro la variabile somma, altrimenti sarà undefined
+let somma = 0;
+let media;
+
+for (let i = 0; i <= 10; i++) {
+    // incremento
+    somma += i;
+    // faccio la media
+    media = somma / 10;
+}
+
+// stampo il risultato
+console.log("La somma dei numeri da 1 a 10 è", somma, "e la media è", media);
+document.getElementById('message').innerHTML = `La somma dei numeri da 1 a 10 è ${somma} e la media è ${media}.`;
