ca627fc6d64d5f2ab0013509400dc50c4b8606fd
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-19 14:12:50 +0200

    snack 1  sviluppato

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,3 +1,28 @@
 // L'utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
+// chiedo all’utente la prima parola
+const firstWord = prompt("Inserisci una parola");
+console.log(firstWord);
+
+// chiedo all’utente la seconda parola
+const secondWord = prompt("Inserisci un'altra parola");
+console.log(secondWord);
+
+// calcolo lunghezza parole
+const firstLenght = firstWord.length;
+const secondLenght = secondWord.length;
+
+// confronto le parole
+let result;
+if (firstLenght < secondLenght) {
+    result = `la parola più corta è ${firstWord} e la più lunga è ${secondWord}`;
+} else if (firstLenght > secondLenght) {
+    result = `la parola più corta è ${secondWord} e la più lunga è ${firstWord}`;
+} else {
+    result = `le parole ${firstWord} e ${secondWord} sono uguali`;
+}
+
+// stampo in console e pagina
+console.log(result);
+document.getElementById('message').innerHTML = result;
